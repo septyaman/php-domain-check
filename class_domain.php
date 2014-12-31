6a0212bd18242ed0b9833359fcd5b29264282e33
Septyaman Vom <im@septyaman.com>
2015-01-01 05:04:18 +0700

    Add Class Domain_Checker
    Base Class for php-domain-check

---
+++ class_domain.php
@@ -0,0 +1,26 @@
+<?php
+/*
+   PHP DOMAIN CHECK
+   Check if Domain Is Available
+   source https://github.com/septyaman/php-domain-check
+   
+*/
+
+ class Domain_Checker {
+
+
+
+
+	
+
+	
+	
+	
+	
+	
+ 
+ 
+ 
+ 
+ 
+ }
